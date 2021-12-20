5f658439881f10613206dc3c0a9413b348694bc9
kabiru <mwangikabiru21@gmail.com>
2021-12-21 00:02:08 +0300

    Day 2: Part Two

--- day2/init.py
+++ day2/init.py
@@ -2,6 +2,7 @@ def run_my_boy():
     commands = readFile()
     
     print(f"Part One : {part_one(commands)}")
+    print(f"Part Two : {part_two(commands)}")
 
 
 def part_one(commands):
@@ -16,6 +17,23 @@ def part_one(commands):
             v_pos += int(offset)
         elif direction == "up":
             v_pos -= int(offset)
+        
+    return h_pos * v_pos
+
+
+def part_two(commands):
+    h_pos, v_pos, aim = 0, 0, 0
+
+    for command in commands:
+        direction, offset = command.split(" ")
+                
+        if direction == "forward":
+            h_pos += int(offset)
+            v_pos += aim * int(offset)
+        elif direction == "down":
+            aim += int(offset)
+        elif direction == "up":
+            aim -= int(offset)
         
     return h_pos * v_pos
 
