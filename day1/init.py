dbb1ae701a1a728fcd0d18c5cb962632bc3cf9e1
kabiru <mwangikabiru21@gmail.com>
2021-12-20 19:58:44 +0300

    Day 2: Part Two

--- day1/init.py
+++ day1/init.py
@@ -1,27 +1,43 @@
 def run_my_boy():
-    # Get data from the file 
     depths = readFile()
 
-    # Print the depths like in the example
+    print(f"Part One : {part_one(depths)}")    
+    print(f"Part Two : {part_two(depths, 3)}")
+
+
+def part_one(depths):
     prev_depth = depths[0]
     count_inc = 0
-    print(f"{prev_depth} - (N/A - no previous measurement)")
 
     for depth in depths[1:]:
         if depth > prev_depth:
-            print(f"{depth} (increased)")
             count_inc += 1
-        else:
-            print(f"{depth} (decreased)")
 
         prev_depth = depth
+    
+    return count_inc
+    
 
-    print(f"The total count increase is : {count_inc}")
+def part_two(depths, window_size):
+    prev_depth_sum = sum(depths[0 : window_size])
+    count_inc = 0
+
+    for depth in range(1, len(depths) - (window_size - 1)):
+        _depths_sum = sum(depths[depth : depth + window_size])
+
+        if _depths_sum > prev_depth_sum:
+            count_inc += 1
+        
+        prev_depth_sum = _depths_sum
+
+
+    return count_inc
 
 
 def readFile():
     f = open("input.txt", "r")
     data = f.read().splitlines()
+    data = [ int(x) for x in data ]
     return data 
     
 
