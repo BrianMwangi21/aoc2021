f0d5e952f3ab7daad6a0873696cfdae9f6886180
kabiru <mwangikabiru21@gmail.com>
2021-12-21 21:17:00 +0300

    Day 3: Part Two

--- day3/init.py
+++ day3/init.py
@@ -2,10 +2,10 @@ def run_my_boy():
     diagnostics = readFile()
     
     print(f"Part One : {part_one(diagnostics)}")
+    print(f"Part Two : {part_two(diagnostics)}")
 
 
-def part_one(diagnostics):
-    gamma, epsilon = '', ''
+def get_index_arrs(diagnostics):
     index_arrs = []
     
     for i in range( len(diagnostics[0]) ):
@@ -15,7 +15,14 @@ def part_one(diagnostics):
         split = list(diagnostic)
         for i in range(len(split)):
             index_arrs[i].append(split[i])
-            
+    
+    return index_arrs
+
+
+def part_one(diagnostics):
+    gamma, epsilon = '', ''
+    index_arrs = get_index_arrs(diagnostics)
+
     # Get gamma and epsilon of each array
     for index_arr in index_arrs:
         gamma += get_gamma( index_arr )
@@ -26,9 +33,23 @@ def part_one(diagnostics):
     epsilon = int(epsilon, 2)
     power_consumption = gamma * epsilon
 
-    print(f"Gamma: {gamma}, Epsilon: {epsilon} = Power Consumption {power_consumption}")
-
     return power_consumption
+
+
+def part_two(diagnostics):
+    oxygen_rating, c02_scrubber = '', ''
+    index_arrs = get_index_arrs(diagnostics)
+            
+    # Get o2 rating and c02 scrubber
+    oxygen_rating = get_oxygen_rating(diagnostics, index_arrs, 0)
+    c02_scrubber = get_c02_rating(diagnostics, index_arrs, 0)
+
+    # Convert to interger
+    oxygen_rating = int(oxygen_rating, 2)
+    c02_scrubber = int(c02_scrubber, 2)
+    life_support_rating = oxygen_rating * c02_scrubber
+
+    return life_support_rating
 
    
 def get_gamma(arr):
@@ -40,10 +61,30 @@ def get_gamma(arr):
         elif i == '1':
             c_one += 1
 
-    if c_one > c_zero:
+    if c_one >= c_zero:
         return "1"
     else:
         return "0"
+
+
+def get_oxygen_rating(diagnostics, index_arrs, index):
+    if index < len(index_arrs):
+        gamma = get_gamma( index_arrs[index] ) 
+
+        # Now check if matches gamma
+        new_diagnostics = []
+        for i in range( len(diagnostics) ):
+            if diagnostics[i][index] == gamma:
+                new_diagnostics.append( diagnostics[i] ) 
+
+        index_arrs = get_index_arrs(new_diagnostics)
+
+        if len(new_diagnostics) == 1:
+            return new_diagnostics[0]
+        else:
+            return get_oxygen_rating( new_diagnostics, index_arrs, index + 1)
+    else:
+        return diagnostics[0]
 
 
 def get_epsilon(arr):
@@ -55,10 +96,30 @@ def get_epsilon(arr):
         elif i == '1':
             c_one += 1
 
-    if c_one < c_zero:
-        return "1"
-    else:
+    if c_zero <= c_one:
         return "0"
+    else:
+        return "1"
+
+
+def get_c02_rating(diagnostics, index_arrs, index):
+    if index < len(index_arrs):
+        epsilon = get_epsilon( index_arrs[index] ) 
+
+        # Now check if matches gamma
+        new_diagnostics = []
+        for i in range( len(diagnostics) ):
+            if diagnostics[i][index] == epsilon:
+                new_diagnostics.append( diagnostics[i] ) 
+        
+        index_arrs = get_index_arrs(new_diagnostics)
+
+        if len(new_diagnostics) == 1:
+            return new_diagnostics[0]
+        else:
+            return get_c02_rating( new_diagnostics, index_arrs, index + 1)
+    else:
+        return diagnostics[0]
 
 
 def readFile():
