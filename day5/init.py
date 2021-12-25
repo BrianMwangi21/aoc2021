b08531462f93a6ffd56d87c2e0c4639130d5cd8b
kabiru <mwangikabiru21@gmail.com>
2021-12-25 23:44:22 +0300

    Day 5: Part Two [blunder]

--- day5/init.py
+++ day5/init.py
@@ -2,20 +2,19 @@ def run_my_boy():
     coordinates, size_x, size_y = readFile()
     
     print(f"Part One : {part_one(coordinates, size_x, size_y)}")
+    print(f"Part Two : {part_two(coordinates, size_x, size_y)}")
 
 
 def part_one(coordinates, size_x, size_y):
-    board = [[0] * (size_x) for _ in range( size_y )] 
+    board = [[0] * (size_y) for _ in range( size_x )] 
 
     # Loop through the coordinates
     for coordinate in coordinates:
         coord_one = coordinate[0].split(",")
         coord_two = coordinate[1].split(",")
-        found_x_y = False
 
         # Check for only horizontal / vertical lines
         if coord_one[0] == coord_two[0]:
-            found_x_y = True
             # Get start point and end point on Y axis
             x = int(coord_one[0])
             start_y = min( int(coord_one[1]) , int(coord_two[1]) )
@@ -24,10 +23,8 @@ def part_one(coordinates, size_x, size_y):
             # Then loop appending the value in the row
             for col in range(size_y):
                 if col >= start_y and col <= end_y:
-                    board[col][x] += 1
-
+                    board[x][col] += 1
         elif coord_one[1] == coord_two[1]:
-            found_x_y = True
             # Get start point and end point on X axis
             y = int(coord_one[1])
             start_x = min( int(coord_one[0]) , int(coord_two[0]) )
@@ -36,17 +33,73 @@ def part_one(coordinates, size_x, size_y):
             # Then loop appending the value in the row
             for row in range(size_x):
                 if row >= start_x and row <= end_x:
-                    board[y][row] += 1
-    
+                    board[row][y] += 1
+
     # Now count occurences of > 2
     overlap_count = 0
-    for row in range(size_y):
-        for col in range(size_x):
+    for row in range(size_x):
+        for col in range(size_y):
             if board[row][col] >= 2:
                 overlap_count += 1
     
-    print(f"For all coordinates, 2 or more lines overlap {overlap_count} times")
     return overlap_count
+
+def part_two(coordinates, size_x, size_y):
+    board = [[0] * (size_y) for _ in range( size_x )] 
+
+    # Loop through the coordinates
+    for coordinate in coordinates:
+        coord_one = coordinate[0].split(",")
+        coord_two = coordinate[1].split(",")
+        
+        # Check for only horizontal / vertical lines
+        if coord_one[0] == coord_two[0]:
+            # Get start point and end point on Y axis
+            x = int(coord_one[0])
+            start_y = min( int(coord_one[1]) , int(coord_two[1]) )
+            end_y = max( int(coord_one[1]) , int(coord_two[1]) )
+
+            # Then loop appending the value in the row
+            for col in range(size_y):
+                if col >= start_y and col <= end_y:
+                    board[x][col] += 1
+        elif coord_one[1] == coord_two[1]:
+            # Get start point and end point on X axis
+            y = int(coord_one[1])
+            start_x = min( int(coord_one[0]) , int(coord_two[0]) )
+            end_x = max( int(coord_one[0]) , int(coord_two[0]) )
+
+            # Then loop appending the value in the row
+            for row in range(size_x):
+                if row >= start_x and row <= end_x:
+                    board[row][y] += 1
+        else:
+            x1, x2 = int(coord_one[0]), int(coord_two[0])
+            y1, y2 = int(coord_one[1]), int(coord_two[1])
+
+            # Check for absolute difference
+            x_diff= abs( x1 - x2 )
+            y_diff = abs( y1 - y2 )
+
+            if x_diff == y_diff:
+                x_step = 1 if x1 < x2 else -1
+                y_step = 1 if y1 < y2 else -1
+                t_steps = max( x_diff, y_diff )
+
+                for _ in range(t_steps + 1):
+                    board[x1][y1] += 1
+                    x1 += x_step
+                    y1 += y_step
+    
+    # Now count occurences of > 2
+    overlap_count = 0
+    for row in range(size_x):
+        for col in range(size_y):
+            if board[row][col] >= 2:
+                overlap_count += 1
+    
+    return overlap_count
+    
 
 def print_board(board):
     for x in board:
@@ -73,7 +126,6 @@ def readFile():
         size_y = max( size_y, int(coord_one[1]) )
         size_y = max( size_y, int(coord_two[1]) )
 
-    print(f"After reading data, the board size is {size_x} by {size_y}")
 
     return coordinates, ( size_x + 1 ) , ( size_y + 1 )
 
