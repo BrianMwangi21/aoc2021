7d4db20b683224497db5f4284be0ac35cb8358d4
kabiru <mwangikabiru21@gmail.com>
2021-12-27 18:53:00 +0300

    Day 7: Part 2 Efficient

--- day7/init.py
+++ day7/init.py
@@ -2,10 +2,10 @@ def run_my_boy():
     crab_data, min_pos, max_pos = readFile()
 
     print(f"Part One : {part_one(crab_data, min_pos, max_pos)}")
+    print(f"Part Two : {part_two(crab_data, min_pos, max_pos)}")
 
 def part_one(crab_data, min_pos, max_pos):
     sum_of_fuels = []
-    sum_of_fuels_only = []
 
     for x in range(min_pos, max_pos + 1):
         sum_of_fuel = 0
@@ -13,15 +13,25 @@ def part_one(crab_data, min_pos, max_pos):
         for location in crab_data:
             sum_of_fuel += abs( x - location )
 
-        sum_of_fuels.append({
-            "sum" : sum_of_fuel,
-            "pos" : x,
-            "desc": f"Sum of fuel to location {x} is {sum_of_fuel}"
-        })
-        sum_of_fuels_only.append( sum_of_fuel )
+        sum_of_fuels.append( sum_of_fuel )
     
     
-    return min(sum_of_fuels_only)
+    return min(sum_of_fuels)
+
+def part_two(crab_data, min_pos, max_pos):
+    sum_of_fuels = []
+
+    for x in range(min_pos, max_pos + 1):
+        sum_of_fuel = 0
+
+        for location in crab_data:
+            diff = abs( x - location )
+            sum_of_fuel += diff * ( diff + 1 ) // 2
+
+        sum_of_fuels.append( sum_of_fuel )
+    
+    
+    return min(sum_of_fuels)
 
 def readFile():
     with open("input.txt") as f:
